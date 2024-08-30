2fdcb9a671f1bba3a3bb6105ed34e3e2aa402485
leehyoin <lhi7247@gmail.com>
2024-08-30 11:38:28 +0900

    aos 수정중

--- js/introduction.js
+++ js/introduction.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  AOS.init({
-    duration: 1000,  // 애니메이션 지속 시간 (밀리초)
-    offset: 100,    // 애니메이션이 시작되는 오프셋
-    once: true,     // 스크롤 시 애니메이션이 한 번만 발생하도록 설정
-  });
+  AOS.init({});
+    // 다음 섹션 이동
+    $(".scroll_btn").click(function(){
+      window.scrollTo({
+        top: 2100,
+        behavior: "smooth",
+      })
+    })
 });
