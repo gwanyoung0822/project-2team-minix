b8c5e0de28ab93730e505c2b8a7d15b7ead247a5
leehyoin <lhi7247@gmail.com>
2024-09-02 17:02:02 +0900

    sec 1이미지 대체, 반응형 sec1,2 조정 중

--- js/introduction.js
+++ js/introduction.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  AOS.init({
-  }); 
-  // 다음 섹션 이동
+  AOS.init({});
+
   $(".scroll_btn").click(function () {
+    // sec1_copy_wrap 요소를 선택합니다.
+    const sec1CopyWrap = document.querySelector(".sec1_copy_wrap");
+
+    // sec1_copy_wrap 요소의 최상단 위치를 계산합니다.
+    const sec1CopyWrapTop = sec1CopyWrap.offsetTop;
+
+    // sec1_copy_wrap 요소의 최상단으로 부드럽게 스크롤합니다.
     window.scrollTo({
-      top: 2100,
+      top: sec1CopyWrapTop,
       behavior: "smooth",
     });
   });
