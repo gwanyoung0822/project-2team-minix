1244b1994b8881344b9a523b0c62e9e261c1eaae
leehyoin <lhi7247@gmail.com>
2024-09-05 17:46:24 +0900

    상세페이지 pp-desc - html,css,js수정

--- js/pp_desc.js
+++ js/pp_desc.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function toggleOpen() {
         // 모든 갤러리 아이템에서 'open' 클래스 제거
         galleryItems.forEach(item => item.classList.remove('open'));
-
+        
         // 모든 텍스트 아이템에서 'open-text'와 'shrink-text' 클래스 제거
         textItems.forEach(item => {
             item.classList.remove('open-text');
@@ -25,7 +25,30 @@ document.addEventListener('DOMContentLoaded', function() {
                 item.classList.add('shrink-text');
             }
         });
+
+        // 모든 갤러리 아이템에서 'paused' 클래스 추가
+        galleryItems.forEach(item => item.classList.add('paused'));
+
+        // 현재 갤러리 아이템의 'paused' 클래스를 제거
+        this.classList.remove('paused');
     }
 
-    galleryItems.forEach(item => item.addEventListener('click', toggleOpen));
+    function resetItems() {
+        // 모든 갤러리 아이템에서 'open' 클래스 제거 및 'paused' 클래스 제거
+        galleryItems.forEach(item => {
+            item.classList.remove('open');
+            item.classList.remove('paused');
+        });
+
+        // 모든 텍스트 아이템에서 'open-text'와 'shrink-text' 클래스 제거
+        textItems.forEach(item => {
+            item.classList.remove('open-text');
+            item.classList.remove('shrink-text');
+        });
+    }
+
+    galleryItems.forEach(item => {
+        item.addEventListener('mouseover', toggleOpen);
+        item.addEventListener('mouseout', resetItems);
+    });
 });
