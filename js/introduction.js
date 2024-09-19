8bf0cfde7488e1309bf2801fb0668ff21aa8ff6b
leehyoin <lhi7247@gmail.com>
2024-09-19 16:32:16 +0900

    갤러리 수정, 768이하 스와이퍼 적용중, 구매 버튼 크기, 간격 수정,

--- js/introduction.js
+++ js/introduction.js
@@ -19,57 +19,51 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   // ===================================갤러리====================================================
+  // 갤러리 스와이퍼
   
+  // 갤러리 호버 애드클래스
   function toggleOpen() {
     // 모든 갤러리 아이템에서 'open' 클래스 제거
     galleryItems.forEach((item) => item.classList.remove("open"));
-
     // 모든 텍스트 아이템에서 'open-text'와 'shrink-text' 클래스 제거
     textItems.forEach((item) => {
       item.classList.remove("open-text");
       item.classList.remove("shrink-text");
     });
-
     // 클릭된 갤러리 아이템과 해당 인덱스의 텍스트 아이템에 'open'과 'open-text' 클래스 추가
     this.classList.add("open");
     const index = Array.from(galleryItems).indexOf(this);
     if (textItems[index]) {
       textItems[index].classList.add("open-text");
     }
-
     // 나머지 텍스트 아이템에 'shrink-text' 클래스 추가
     textItems.forEach((item, idx) => {
       if (idx !== index) {
         item.classList.add("shrink-text");
       }
     });
-
     // 모든 갤러리 아이템에서 'paused' 클래스 추가
     galleryItems.forEach((item) => item.classList.add("paused"));
-
     // 현재 갤러리 아이템의 'paused' 클래스를 제거
     this.classList.remove("paused");
   }
-  
-
   function resetItems() {
     // 모든 갤러리 아이템에서 'open' 클래스 제거 및 'paused' 클래스 제거
     galleryItems.forEach((item) => {
       item.classList.remove("open");
       item.classList.remove("paused");
     });
-
     // 모든 텍스트 아이템에서 'open-text'와 'shrink-text' 클래스 제거
     textItems.forEach((item) => {
       item.classList.remove("open-text");
       item.classList.remove("shrink-text");
     });
   }
-
   galleryItems.forEach((item) => {
     item.addEventListener("mouseover", toggleOpen);
     item.addEventListener("mouseout", resetItems);
   });
+
   // ===================================갤러리====================================================
 
   /**
